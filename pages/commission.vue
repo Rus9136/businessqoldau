5273a4ae640d16e87af61abaade42f1d15eb868b
Maraalll <118250379+Maraalll@users.noreply.github.com>
2025-11-26 00:16:37 +0500

    Create commission.vue

---
+++ pages/commission.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="container-custom py-16">
+    <h1 class="text-3xl font-bold mb-6">Состав комиссии</h1>
+    <p class="text-lg text-gray-700 leading-relaxed">
+      Здесь будет информация о комиссии конкурса.
+    </p>
+  </section>
+</template>
